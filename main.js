841634ba863c8837b16417b8d76f480560cd026f
Roman Holovchenko <GolovchenkoR777@gmail.com>
2022-04-13 21:37:42 +0300

    fix modal

--- main.js
+++ main.js
@@ -1,7 +1,28 @@
-"use strict"
+"usestrict"
 
-
-let n = 2; // теперь n = 7 (работает как n = n + 5)
-n *= 2; // теперь n = 14 (работает как n = n * 2)
-
-alert( n ); // 14
+let modal = document.querySelector(".modal");
+let backdrop = document.querySelector(".backdrop");
+let buttonOpen = document.querySelector(".hero__button");
+let buttonClose = document.querySelector(".modal__close");
+buttonOpen.addEventListener("click", function () {
+    modal.classList.add("modal--open")
+    backdrop.classList.add("backdrop--show")
+});
+buttonClose.addEventListener("click", function () {
+    modal.classList.remove("modal--open")
+    backdrop.classList.remove("backdrop--show")
+});
+// CHECKBOX
+let checkbox = document.querySelector(".modal__checkbox");
+let checkboxActive = document.querySelector(".modal__checkbox--active")
+checkbox.addEventListener("click", function () {
+    console.log("dsadasdas")
+    checkboxActive.classList.add("modal__checkbox--active-show");
+});
+checkboxActive.addEventListener("click", function () {
+    console.log("ZALUPA")
+    checkboxActive.classList.remove("modal__checkbox--active-show")
+})
+// checkboxActive.addEventListener("click", function () {
+//     console.log("zakrivay")
+// });
