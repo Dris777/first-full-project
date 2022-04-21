daeb343b734c81b4732fa7732afc619569def3b2
Roman Holovchenko <GolovchenkoR777@gmail.com>
2022-04-21 21:58:55 +0300

    fix

--- burger.js
+++ burger.js
@@ -1,13 +1,16 @@
+"use strict"
 let burgerOpen = document.querySelector(".burger-menu__icon-open");
 let burgerMenu = document.querySelector(".burger");
 let burgerNav = document.querySelector(".nav__list");
 let burgerAddress = document.querySelector(".header__list");
 let burgerClose = document.querySelector(".burger-menu__icon-close");
+let wrap = document.querySelector(".wrap");
 burgerOpen.addEventListener("click", function () {
     burgerOpen.classList.add("burger-menu__icon-open--hide")
     burgerMenu.classList.add("burger-menu__page--open")
     burgerNav.classList.add("nav__list--show")
     burgerClose.classList.add("burger-menu__icon-close--show")
+    wrap.classList.add("wrap--hide")
 })
 
 burgerClose.addEventListener("click", function () {
@@ -15,4 +18,5 @@ burgerClose.addEventListener("click", function () {
     burgerMenu.classList.toggle("burger-menu__page--open")
     burgerNav.classList.toggle("nav__list--show")
     burgerClose.classList.toggle("burger-menu__icon-close--show")
+    wrap.classList.toggle("wrap--hide")
 })
