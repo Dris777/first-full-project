17240013fe84281a6bbe981e4112f654b0736646
Roman Holovchenko <GolovchenkoR777@gmail.com>
2022-04-23 11:20:54 +0300

    add adaptiv

--- burger.js
+++ burger.js
@@ -1,10 +1,12 @@
-"use strict"
+"use-strict"
+
 let burgerOpen = document.querySelector(".burger-menu__icon-open");
 let burgerMenu = document.querySelector(".burger");
 let burgerNav = document.querySelector(".nav__list");
 let burgerAddress = document.querySelector(".header__list");
 let burgerClose = document.querySelector(".burger-menu__icon-close");
 let wrap = document.querySelector(".wrap");
+console.log(wrap)
 burgerOpen.addEventListener("click", function () {
     burgerOpen.classList.add("burger-menu__icon-open--hide")
     burgerMenu.classList.add("burger-menu__page--open")
--- modal.js
+++ modal.js
@@ -1,14 +1,14 @@
-"usestrict"
+"use-strict"
 
 let modal = document.querySelector(".modal");
 let backdrop = document.querySelector(".backdrop");
 let buttonOpen = document.querySelector(".hero__button");
 let buttonClose = document.querySelector(".modal__close");
-let burgerOpen = document.querySelector(".burger-menu__icon-open");
-let burgerMenu = document.querySelector(".burger");
-let burgerNav = document.querySelector(".nav__list");
-let burgerAddress = document.querySelector(".header__list");
-let burgerClose = document.querySelector(".burger-menu__icon-close");
+//let burgerOpen = document.querySelector(".burger-menu__icon-open");
+//let burgerMenu = document.querySelector(".burger");
+//let burgerNav = document.querySelector(".nav__list");
+//let burgerAddress = document.querySelector(".header__list");
+//let burgerClose = document.querySelector(".burger-menu__icon-close");
 buttonOpen.addEventListener("click", function () {
     modal.classList.add("modal--open")
     backdrop.classList.add("backdrop--show")
@@ -34,17 +34,17 @@ checkboxActive.addEventListener("click", function () {
 });
 
 //BURGER MENU 
-burgerOpen.addEventListener("click", function () {
+//burgerOpen.addEventListener("click", function () {
     burgerOpen.classList.add("burger-menu__icon-open--hide")
     burgerMenu.classList.add("burger-menu__page--open")
     burgerNav.classList.add("nav__list--show")
     burgerClose.classList.add("burger-menu__icon-close--show")
-})
+//})
 
-burgerClose.addEventListener("click", function () {
-    burgerOpen.classList.toggle("burger-menu__icon-open--hide")
-    burgerMenu.classList.toggle("burger-menu__page--open")
-    burgerNav.classList.toggle("nav__list--show")
-    burgerClose.classList.toggle("burger-menu__icon-close--show")
-})
+// //burgerClose.addEventListener("click", function () {
+//     burgerOpen.classList.toggle("burger-menu__icon-open--hide")
+//     burgerMenu.classList.toggle("burger-menu__page--open")
+//     burgerNav.classList.toggle("nav__list--show")
+//     burgerClose.classList.toggle("burger-menu__icon-close--show")
+// //})
 
