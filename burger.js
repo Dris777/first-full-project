91d5f254e7a918c93d9bf11521af624f3d90cc7d
Roman Holovchenko <GolovchenkoR777@gmail.com>
2022-04-27 16:42:33 +0300

    fix

--- burger.js
+++ burger.js
@@ -1,24 +1,24 @@
 "use-strict"
 
-let burgerOpen = document.querySelector(".burger-menu__icon-open");
+let burgerOpen = document.querySelector(".header__icon-open");
 let burgerMenu = document.querySelector(".burger");
 let burgerNav = document.querySelector(".nav__list");
 let burgerAddress = document.querySelector(".header__list");
-let burgerClose = document.querySelector(".burger-menu__icon-close");
+let burgerClose = document.querySelector(".header__icon-close");
 let wrap = document.querySelector(".wrap");
 console.log(wrap)
 burgerOpen.addEventListener("click", function () {
-    burgerOpen.classList.add("burger-menu__icon-open--hide")
-    burgerMenu.classList.add("burger-menu__page--open")
+    burgerOpen.classList.add("header__icon-open--hide")
+    burgerMenu.classList.add("header__page--open")
     burgerNav.classList.add("nav__list--show")
-    burgerClose.classList.add("burger-menu__icon-close--show")
+    burgerClose.classList.add("header__icon-close--show")
     wrap.classList.add("wrap--hide")
 })
 
 burgerClose.addEventListener("click", function () {
-    burgerOpen.classList.toggle("burger-menu__icon-open--hide")
-    burgerMenu.classList.toggle("burger-menu__page--open")
+    burgerOpen.classList.toggle("header__icon-open--hide")
+    burgerMenu.classList.toggle("header__page--open")
     burgerNav.classList.toggle("nav__list--show")
-    burgerClose.classList.toggle("burger-menu__icon-close--show")
+    burgerClose.classList.toggle("header__icon-close--show")
     wrap.classList.toggle("wrap--hide")
 })
--- modal.js
+++ modal.js
@@ -4,11 +4,6 @@ let modal = document.querySelector(".modal");
 let backdrop = document.querySelector(".backdrop");
 let buttonOpen = document.querySelector(".hero__button");
 let buttonClose = document.querySelector(".modal__close");
-//let burgerOpen = document.querySelector(".burger-menu__icon-open");
-//let burgerMenu = document.querySelector(".burger");
-//let burgerNav = document.querySelector(".nav__list");
-//let burgerAddress = document.querySelector(".header__list");
-//let burgerClose = document.querySelector(".burger-menu__icon-close");
 buttonOpen.addEventListener("click", function () {
     modal.classList.add("modal--open")
     backdrop.classList.add("backdrop--show")
@@ -23,28 +18,12 @@ buttonClose.addEventListener("click", function () {
 let checkbox = document.querySelector(".modal__checkbox");
 let checkboxActive = document.querySelector(".modal__checkbox--active")
 checkbox.addEventListener("click", function () {
-    console.log("dsadasdas")
+
     checkbox.classList.add("modal__checkbox--show")
     checkboxActive.classList.add("modal__checkbox--active-show");
 });
 checkboxActive.addEventListener("click", function () {
-    console.log("ZALUPA")
+    
     checkboxActive.classList.remove("modal__checkbox--active-show")
     checkbox.classList.remove("modal__checkbox--show")
 });
-
-//BURGER MENU 
-//burgerOpen.addEventListener("click", function () {
-    burgerOpen.classList.add("burger-menu__icon-open--hide")
-    burgerMenu.classList.add("burger-menu__page--open")
-    burgerNav.classList.add("nav__list--show")
-    burgerClose.classList.add("burger-menu__icon-close--show")
-//})
-
-// //burgerClose.addEventListener("click", function () {
-//     burgerOpen.classList.toggle("burger-menu__icon-open--hide")
-//     burgerMenu.classList.toggle("burger-menu__page--open")
-//     burgerNav.classList.toggle("nav__list--show")
-//     burgerClose.classList.toggle("burger-menu__icon-close--show")
-// //})
-
